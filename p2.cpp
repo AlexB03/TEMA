5c0e61c9ac085680926a8e7264301a4491591b4d
AlexB03 <44950208+AlexB03@users.noreply.github.com>
2018-11-15 09:13:16 +0200

    Add files via upload

---
+++ p2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+int n,x,z;
+cout<<"CMMDC a cate nr doriti sa aflati?-->";cin>>n;
+int arr[]{};
+for(x=0;x<n;x++){
+    cout<<"Selectati numerele-->";cin>>z;cout<<endl;
+    arr[x]=z;
+    }
+
+return 0;
+}
